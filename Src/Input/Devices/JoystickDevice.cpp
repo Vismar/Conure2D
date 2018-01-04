7e29586e0d494d4d4082e242a7f78e7db5641f79
Vismar <exanimoteam@gmail.com>
2018-01-04 22:13:45 +0300

    C2D-3: Joystick device

---
+++ Src/Input/Devices/JoystickDevice.cpp
@@ -0,0 +1,147 @@
+#include "JoystickDevice.hpp"
+#include "Utilities/KeyStateHandler.hpp"
+#include "Utilities/Keys/JoystickButtons.hpp"
+#include "Utilities/Keys/JoystickAxises.hpp"
+#include "SFML/Window/Joystick.hpp"
+#include "SFML/Window/Event.hpp"
+
+using namespace Input;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+JoystickDevice::JoystickDevice() 
+    : _buttons(new KeyStateHandler[sf::Joystick::ButtonCount]())
+    , _touchedButtons(new bool[sf::Joystick::ButtonCount]())
+    , _axises(new std::atomic<float>[sf::Joystick::AxisCount]())
+    , _joystickId(-1)
+    , _isConnected(false)
+{ }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+JoystickDevice::~JoystickDevice()
+{
+    delete[] _buttons;
+    delete[] _touchedButtons;
+    delete[] _axises;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void JoystickDevice::SetJoystickId(const unsigned int joystickId)
+{
+    _joystickId = joystickId;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void JoystickDevice::SetJoystickConnectionState(const bool connectionState)
+{
+    _isConnected = connectionState;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+KeyState JoystickDevice::ButtonState(const JoystickButton button) const
+{
+    return _buttons[static_cast<int>(button)].State();
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+float JoystickDevice::JoystickAxisPosition(const JoystickAxis axis) const
+{
+    return _axises[static_cast<int>(axis)];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+std::string JoystickDevice::Name() const
+{
+    return sf::Joystick::getIdentification(_joystickId).name;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+unsigned int JoystickDevice::VendorId() const
+{
+    return sf::Joystick::getIdentification(_joystickId).vendorId;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+unsigned int JoystickDevice::ProductId() const
+{
+    return sf::Joystick::getIdentification(_joystickId).productId;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+bool JoystickDevice::IsConnected() const
+{
+    return _isConnected;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void JoystickDevice::HandleJoystickEvents(const sf::Event& event)
+{
+    switch (event.type)
+    {
+    case sf::Event::EventType::JoystickButtonPressed:
+    case sf::Event::EventType::JoystickButtonReleased:
+        _HandleJoystickButtonEvent(event);
+        break;
+    case sf::Event::EventType::JoystickMoved:
+        _HandleJoystickMoveEvent(event);
+        break;
+    default:
+        // Unexpected parameter
+        break;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void JoystickDevice::UpdateNotTouchedButtons()
+{
+    for (auto i = 0; i < sf::Joystick::ButtonCount; ++i)
+    {
+        if (_touchedButtons[i])
+        {
+            _touchedButtons[i] = false;
+        }
+        else
+        {
+            _buttons[i].UpdateState(KeyState::NotTouched);
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void JoystickDevice::_HandleJoystickButtonEvent(const sf::Event& event)
+{
+    _isConnected = true;
+    // If button was pressed or released, get its code and update state
+    if (event.type == sf::Event::EventType::JoystickButtonPressed)
+    {
+        _buttons[event.mouseButton.button].UpdateState(KeyState::Pressed);
+        _touchedButtons[event.mouseButton.button] = true;
+    }
+    else if (event.type == sf::Event::EventType::JoystickButtonReleased)
+    {
+        _buttons[event.mouseButton.button].UpdateState(KeyState::Released);
+        _touchedButtons[event.mouseButton.button] = true;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void JoystickDevice::_HandleJoystickMoveEvent(const sf::Event& event)
+{
+    _isConnected = true;
+    _axises[event.joystickMove.axis] = event.joystickMove.position / 100.0f;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ Src/Input/Devices/JoystickDevice.hpp
@@ -0,0 +1,124 @@
+#pragma once
+#include <atomic>
+#include <string>
+
+namespace sf { class Event; }
+
+namespace Input
+{
+    enum class KeyState;
+    enum class JoystickButton;
+    enum class JoystickAxis;
+    class KeyStateHandler;
+
+    /*!
+     * \brief Device which handles all joystick actions.
+     */
+    class JoystickDevice
+    {
+    public:
+        JoystickDevice(const JoystickDevice&) = delete;
+        JoystickDevice(JoystickDevice&&) = delete;
+        JoystickDevice& operator=(const JoystickDevice&) = delete;
+        JoystickDevice& operator=(JoystickDevice&&) = delete;
+
+        /*!
+         * \brief Default constructor that initializes inner arrays of buttons.
+         */
+        JoystickDevice();
+
+        /*!
+         * \brief Default destructor that frees initialized inner arrays of buttons.
+         */
+        ~JoystickDevice();
+
+        /*!
+         * \brief Sets inner id of current joystick.
+         * \param joystickId - inner id of joystick that will be used by device to identify itself.
+         */
+        void SetJoystickId(const unsigned int joystickId);
+
+        /*!
+         * \brief Sets connection state of current joystick.
+         * \param connectionState - state of connection.
+         */
+        void SetJoystickConnectionState(const bool connectionState);
+
+        /*!
+         * \brief Returns state of specified joystick button.
+         * \param button - specified mouse joystick to return its state.
+         * \return KeyState which defines in what state specified button is.
+         */
+        KeyState ButtonState(const JoystickButton button) const;
+
+        /*!
+         * \brief Returns position [range: -100.0 .. 100.0] of specified joystick axis.
+         * \param axis - axis, value of which is required.
+         * \return Position of specified axis.
+         */
+        float JoystickAxisPosition(const JoystickAxis axis) const;
+
+        /*!
+         * \brief Returns name of current joystick.
+         * \return Vendors name via string,
+         */
+        std::string Name() const;
+
+        /*!
+         * \brief Returns vendor id of current joystick.
+         * \return Id of vendor.
+         */
+        unsigned int VendorId() const;
+
+        /*!
+         * \brief Returns product id of current joystick.
+         * \return Id of product.
+         */
+        unsigned int ProductId() const;
+
+        /*!
+         * \brief Check if current joystick is connected.
+         * \return True if joystick connected. Otherwise - false.
+         */
+        bool IsConnected() const;
+
+        /*!
+         * \brief Handles joystick events.
+         * \param event - joystick event to handle.
+         */
+        void HandleJoystickEvents(const sf::Event& event);
+
+        /*!
+         * \brief Updates state of buttons that was not touched during the normal update.
+         * 
+         * Joystick device stores additional array of buttons that was updated during the update fase.
+         * So, when this function is called, it will updates all buttons that was not updated and resets that additional array.
+         * This function should be executed only after all events was handled during the update fase.
+         */
+        void UpdateNotTouchedButtons();
+
+    private:
+        /*!
+         * \brief Handles joystick button events.
+         * \param event - joystick button event.
+         */
+        void _HandleJoystickButtonEvent(const sf::Event& event);
+
+        /*!
+         * \brief Handles joystick axis movement event.
+         * \param event - joystick axis movement event.
+         */
+        void _HandleJoystickMoveEvent(const sf::Event& event);
+
+        /*! Buttons states. */
+        KeyStateHandler* _buttons;
+        /*! Additional array to know what buttons was updated. */
+        bool* _touchedButtons;
+        /*! Axis states. */
+        std::atomic<float>* _axises;
+        /*! Inner joystick id which is required to identify itself. */
+        unsigned int _joystickId;
+        /*! Flag that defines if current joystick is connected. */
+        std::atomic<bool> _isConnected;
+    };
+}
