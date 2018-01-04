784d8d6ee9d0463fa2073aa1ec1f156a517a78be
Vismar <exanimoteam@gmail.com>
2018-01-04 22:13:25 +0300

    C2D-2: Mouse device

---
+++ Src/Input/Devices/MouseDevice.cpp
@@ -0,0 +1,149 @@
+#include "MouseDevice.hpp"
+#include "Utilities/KeyStateHandler.hpp"
+#include "Utilities/Keys/MouseButtons.hpp"
+#include "SFML/Window/Mouse.hpp"
+#include "SFML/Window/Event.hpp"
+
+using namespace Input;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+MouseDevice::MouseDevice()
+    : _buttons(new KeyStateHandler[sf::Mouse::ButtonCount]())
+    , _touchedButtons(new bool[sf::Mouse::ButtonCount]())
+    ,_verticalWheelDiff(0.0f)
+    , _horizontalWheelDiff(0.0f)
+    , _posX(0)
+    , _posY(0)
+{ }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+MouseDevice::~MouseDevice()
+{
+    delete[] _buttons;
+    delete[] _touchedButtons;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+KeyState MouseDevice::ButtonState(MouseButton button) const
+{
+    return _buttons[static_cast<int>(button)].State();
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+float MouseDevice::VerticalWheelDiff() const
+{
+    return _verticalWheelDiff;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+float MouseDevice::HorizontalWheelDiff() const
+{
+    return _horizontalWheelDiff;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int MouseDevice::PositionX() const
+{
+    return _posX;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int MouseDevice::PositionY() const
+{
+    return _posY;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void MouseDevice::HandleMouseEvent(const sf::Event& event)
+{
+    switch (event.type)
+    {
+    case sf::Event::EventType::MouseButtonPressed:
+    case sf::Event::EventType::MouseButtonReleased:
+        _HandleMouseButtonEvent(event);
+        break;
+    case sf::Event::EventType::MouseWheelScrolled:
+        _HandleMouseWheelEvent(event);
+        break;
+    case sf::Event::EventType::MouseMoved:
+        _HandleMouseMoveEvent(event);
+        break;
+    default:
+        // Unexpected parameter
+        break;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void MouseDevice::UpdateNotTouchedButtons()
+{
+    for (auto i = 0; i < sf::Mouse::ButtonCount; ++i)
+    {
+        if (_touchedButtons[i])
+        {
+            _touchedButtons[i] = false;
+        }
+        else
+        {
+            _buttons[i].UpdateState(KeyState::NotTouched);
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void MouseDevice::UpdateWheels()
+{
+    _verticalWheelDiff = 0.0f;
+    _horizontalWheelDiff = 0.0f;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void MouseDevice::_HandleMouseButtonEvent(const sf::Event& event)
+{
+    // If button was pressed or released, get its code and update state
+    if (event.type == sf::Event::EventType::MouseButtonPressed)
+    {
+        _buttons[event.mouseButton.button].UpdateState(KeyState::Pressed);
+        _touchedButtons[event.mouseButton.button] = true;
+    }
+    else if (event.type == sf::Event::EventType::MouseButtonReleased)
+    {
+        _buttons[event.mouseButton.button].UpdateState(KeyState::Released);
+        _touchedButtons[event.mouseButton.button] = true;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void MouseDevice::_HandleMouseWheelEvent(const sf::Event& event)
+{
+    if (event.mouseWheelScroll.wheel == sf::Mouse::Wheel::VerticalWheel)
+    {
+        _verticalWheelDiff = event.mouseWheelScroll.delta;
+    }
+    else
+    {
+        _horizontalWheelDiff = event.mouseWheelScroll.delta;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void MouseDevice::_HandleMouseMoveEvent(const sf::Event& event)
+{
+    _posX = event.mouseMove.x;
+    _posY = event.mouseMove.y;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ Src/Input/Devices/MouseDevice.hpp
@@ -0,0 +1,116 @@
+#pragma once
+#include <atomic>
+
+namespace sf { class Event; }
+
+namespace Input
+{
+    enum class KeyState;
+    enum class MouseButton;
+    class KeyStateHandler;
+
+    /*!
+     * \brief Device which handles all mouse actions.
+     */
+    class MouseDevice
+    {
+    public:
+        MouseDevice(const MouseDevice&) = delete;
+        MouseDevice(MouseDevice&&) = delete;
+        MouseDevice& operator=(const MouseDevice&) = delete;
+        MouseDevice& operator=(MouseDevice&&) = delete;
+
+        /*!
+         * \brief Default constructor that initializes inner arrays of buttons.
+         */
+        MouseDevice();
+
+        /*!
+         * \brief Default destructor that frees initialized inner arrays of buttons.
+         */
+        ~MouseDevice();
+
+        /*!
+         * \brief Returns state of specified mouse button.
+         * \param button - specified mouse button to return its state.
+         * \return KeyState which defines in what state specified button is.
+         */
+        KeyState ButtonState(MouseButton button) const;
+
+        /*!
+         * \brief Returns wheel offset (positive is up, negative is down).
+         * \return Abstract value that defines how much vertical wheel was scrolled.
+         */
+        float VerticalWheelDiff() const;
+
+        /*!
+         * \brief Returns wheel offset (positive is left, negative is right).
+         * \return Abstract value that defines how much horizontal wheel was scrolled.
+         */
+        float HorizontalWheelDiff() const;
+
+        /*!
+         * \brief Returns current x coordinate of the mouse cursor relative to the top left corner of the window.
+         * \return Integer value of x coordinate of the mouse cursor.
+         */
+        int PositionX() const;
+
+        /*!
+         * \brief Returns current y coordinate of the mouse cursor relative to the top left corner of the window.
+         * \return Integer value of y coordinate of the mouse cursor.
+         */
+        int PositionY() const;
+
+        /*!
+         * \brief Handles mouse events.
+         * \param event - mouse event to handle.
+         */
+        void HandleMouseEvent(const sf::Event& event);
+
+        /*!
+         * \brief Updates state of buttons that was not touched during the normal update.
+         * 
+         * Mouse device stores additional array of buttons that was updated during the update fase.
+         * So, when this function is called, it will updates all buttons that was not updated and resets that additional array.
+         * This function should be executed only after all events was handled during the update fase.
+         */
+        void UpdateNotTouchedButtons();
+
+        /*!
+         * \brief Simply resets mouse wheel scroll value.
+         */
+        void UpdateWheels();
+
+    private:
+        /*!
+         * \brief Handles mouse button events.
+         * \param event - mouse button event.
+         */
+        void _HandleMouseButtonEvent(const sf::Event& event);
+
+        /*!
+         * \brief Handles mouse wheel events.
+         * \param event - mouse wheel event.
+         */
+        void _HandleMouseWheelEvent(const sf::Event& event);
+
+        /*!
+         * \brief Handles mouse move event.
+         * \param event - mouse move event.
+         */
+        void _HandleMouseMoveEvent(const sf::Event& event);
+
+        /*! Buttons states. */
+        KeyStateHandler* _buttons;
+        /*! Additional array to know what buttons was updated. */
+        bool* _touchedButtons;
+        /*! Value of vertical wheel scroll. */
+        std::atomic<float> _verticalWheelDiff;
+        /*! Value of horizontal wheel scroll. */
+        std::atomic<float> _horizontalWheelDiff;
+        /*! Current x coordinate of the mouse cursor relative to the top left corner of the window. */
+        std::atomic<int> _posX;
+        /*! Current y coordinate of the mouse cursor relative to the top left corner of the window. */
+        std::atomic<int> _posY;
+    };
+}
