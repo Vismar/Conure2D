96bb4dc83baddb68af551f195f99f759b5643dde
Vismar <exanimoteam@gmail.com>
2018-01-04 22:16:54 +0300

    C2D-9: InputMapManager

---
+++ Src/Input/Managers/InputMapManager.cpp
@@ -0,0 +1,95 @@
+#include "InputMapManager.hpp"
+#include "Utilities/MappedInput/InputAction.hpp"
+#include "Utilities/MappedInput/InputAxis.hpp"
+
+using namespace Input;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+InputMapManager::InputMapManager()
+    : _inputActionMap(new std::unordered_map<std::string, InputAction>)
+    , _inputAxisMap(new std::unordered_map<std::string, InputAxis>)
+{
+    
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+InputMapManager::~InputMapManager()
+{
+    delete _inputActionMap;
+    delete _inputAxisMap;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void InputMapManager::MapAction(const std::string& actionName, const KeyState state, const KeyboardKey key)
+{
+    (*_inputActionMap)[actionName].SetRequiresState(state);
+    (*_inputActionMap)[actionName].SetRequriedButton(-1, ActionButton(key));
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void InputMapManager::MapAction(const std::string& actionName, const KeyState state, const MouseButton mouseButton)
+{
+    (*_inputActionMap)[actionName].SetRequiresState(state);
+    (*_inputActionMap)[actionName].SetRequriedButton(-1, ActionButton(mouseButton));
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void InputMapManager::MapAction(const std::string& actionName, const KeyState state, const int joystickId, const JoystickButton joystickButton)
+{
+    (*_inputActionMap)[actionName].SetRequiresState(state);
+    (*_inputActionMap)[actionName].SetRequriedButton(joystickId, ActionButton(joystickButton));
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void InputMapManager::MapAxis(const std::string& axisName, const int joystickId, const JoystickAxis axis)
+{
+    (*_inputAxisMap)[axisName].SetAxis(joystickId, axis);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void InputMapManager::MapAxis(const std::string& axisName, const std::string& negativeAction, const std::string& positiveAction)
+{
+    (*_inputAxisMap)[axisName].SetAxis(negativeAction, positiveAction);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+bool InputMapManager::IsActionActive(const std::string& actionName) const
+{
+    bool isActive(false);
+
+    const auto action = _inputActionMap->find(actionName);
+
+    // Check if such action exist
+    if (action != _inputActionMap->end())
+    {
+        isActive = action->second.IsActive();
+    }
+
+    return isActive;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+float InputMapManager::GetAxisPosition(const std::string& axisName) const
+{
+    float axisPosition(0.0f);
+
+    // Check if such axis exist
+    const auto axis = _inputAxisMap->find(axisName);
+    if (axis != _inputAxisMap->end())
+    {
+        axisPosition = axis->second.GetPosition();
+    }
+
+    return axisPosition;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ Src/Input/Managers/InputMapManager.hpp
@@ -0,0 +1,107 @@
+#pragma once
+#include <string>
+#include <unordered_map>
+
+namespace  Input
+{
+    enum class KeyState;
+    enum class KeyboardKey;
+    enum class MouseButton;
+    enum class JoystickButton;
+    enum class JoystickAxis;
+    class InputAction;
+    class InputAxis;
+
+    /*!
+     * \brief Manager that handles creating/updating/deleting of mapped input actions and axises.
+     */
+    class InputMapManager
+    {
+    public:
+        InputMapManager(const InputMapManager&) = delete;
+        InputMapManager(InputMapManager&&) = delete;
+        InputMapManager& operator=(const InputMapManager&) = delete;
+        InputMapManager& operator=(InputMapManager&&) = delete;
+
+        /*!
+         * \brief Default constructor that initializes maps of input actions and input axises.
+         */
+        InputMapManager();
+
+        /*!
+         * \brief Default destructor that deletes mapf of input actions and input axises.
+         */
+        ~InputMapManager();
+
+        /*!
+         * \brief Map specified action to a certain name.
+         * \param actionName - name of action that will be used. 
+         * \param state - key state to activate action.
+         * \param key - keyboard key to check.
+         * 
+         * Only one action can be assigned to a certain name. If name was already used, its button will be rewritten.
+         */
+        void MapAction(const std::string& actionName, const KeyState state, const KeyboardKey key);
+
+        /*!
+         * \brief Map specified action to a certain name.
+         * \param actionName - name of action that will be used. 
+         * \param state - button state to activate action.
+         * \param mouseButton - mouse button to check.
+         * 
+         * Only one action can be assigned to a certain name. If name was already used, its button will be rewritten.
+         */
+        void MapAction(const std::string& actionName, const KeyState state, const MouseButton mouseButton);
+
+        /*!
+         * \brief Map specified action to a certain name.
+         * \param actionName - name of action that will be used. 
+         * \param state - button state to activate action.
+         * \param joystickId - id of joystick to check.
+         * \param joystickButton - joystick button to check.
+         * 
+         * Only one action can be assigned to a certain name. If name was already used, its button will be rewritten.
+         */
+        void MapAction(const std::string& actionName, const KeyState state, const int joystickId, const JoystickButton joystickButton);
+
+        /*!
+         * \brief Map specified axis to a certain name.
+         * \param axisName - name of axis that will be used. 
+         * \param joystickId - id of joystick to check.
+         * \param axis - axis to check.
+         * 
+         * Only one axis can be assigned to a certain name. If name was already used, its axis will be rewritten.
+         */
+        void MapAxis(const std::string& axisName, const int joystickId, const JoystickAxis axis);
+
+        /*!
+         * \brief Map specified axis to a certain name.
+         * \param axisName - name of axis that will be used. 
+         * \param negativeAction - name of the action that will be interpreted as the negative position of an axis.
+         * \param positiveAction - name of the action that will be interpreted as the positive position of an axis.
+         * 
+         * Only one axis can be assigned to a certain name. If name was already used, its axis will be rewritten.
+         */
+        void MapAxis(const std::string& axisName, const std::string& negativeAction, const std::string& positiveAction);
+
+        /*!
+         * \brief Checks if any of associated with the named action input actions is active.
+         * \param actionName -  name to associate with.
+         * \return True if at least one input action is active.
+         */
+        bool IsActionActive(const std::string& actionName) const;
+
+        /*!
+         * \brief Returns the position of the biggest by absolute value input axis.
+         * \param axisName -  name to associate with.
+         * \return The position of the biggest by absolute value input axis.
+         */
+        float GetAxisPosition(const std::string& axisName) const;
+
+    private:
+        /*! Map of named actions that stores array of input actions. */
+        std::unordered_map<std::string, InputAction>* _inputActionMap;
+        /*! Map of named axises that stores array of input axises. */
+        std::unordered_map<std::string, InputAxis>* _inputAxisMap;
+    };
+}
