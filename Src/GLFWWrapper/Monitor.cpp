e8d021ede133f54b39fb51be7fc071a27c423575
Vismar <exanimoteam@gmail.com>
2020-11-29 03:06:00 +0300

    C2D-43: Create Monitor class that abstracts GLFWmonitor

---
+++ Src/GLFWWrapper/Monitor.cpp
@@ -0,0 +1,73 @@
+#include "Monitor.hpp"
+#include <Utility/Assert.hpp>
+
+using namespace GLFWWrapper;
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+Monitor::Monitor(GLFWmonitor* monitor)
+: _monitor(monitor)
+{
+    Assert(_monitor != nullptr, "Provided monitor is nullptr");
+
+    int32_t numberOfVideoModes;
+    auto* videoModes = glfwGetVideoModes(_monitor, &numberOfVideoModes);
+    VideoMode selectedVideoMode(*glfwGetVideoMode(_monitor));
+
+    for (auto i = 0; i < numberOfVideoModes; ++i)
+    {
+        _videoModes.emplace_back(VideoMode(videoModes[i]));
+        if (_videoModes.back() == selectedVideoMode)
+        {
+            _selectedVideoMode = static_cast<size_t>(i);
+        }
+    }
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+std::string_view Monitor::GetName() const
+{
+    return glfwGetMonitorName(_monitor);
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+const std::vector<VideoMode>& Monitor::GetVideoModes() const
+{
+    return _videoModes;
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+void Monitor::SelectVideoMode(size_t videoModeNumber)
+{
+    Assert(videoModeNumber < _videoModes.size(),
+           "Specified videoModeNumber is bigger or equal than the number of video modes");
+    _selectedVideoMode = videoModeNumber;
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+const VideoMode& Monitor::GetSelectedVideoMode() const
+{
+    return _videoModes[_selectedVideoMode];
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+std::tuple<int32_t, int32_t> Monitor::GetContentScale() const
+{
+    int32_t xScale, yScale;
+    glfwGetMonitorPhysicalSize(_monitor, &xScale, &yScale);
+    return std::tie(xScale, yScale);
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+GLFWmonitor* Monitor::GetHandle() const
+{
+    return _monitor;
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
---
+++ Src/GLFWWrapper/Monitor.hpp
@@ -0,0 +1,95 @@
+#pragma once
+#include <string_view>
+#include <vector>
+#include <tuple>
+#include <GLFW/glfw3.h>
+#include <GLFWWrapper/VideoMode.hpp>
+
+namespace GLFWWrapper
+{
+    /*!
+     * A wrapper class for the GLFWmonitor.
+     */
+    class Monitor final
+    {
+    public:
+        /*!
+         * Creates a wrapper around the provided GLFWmonitor.
+         *
+         * \param monitor Raw pointer to the GLFWmonitor for which a wrapper will be created.
+         *
+         * \attention If provided pointer if nullptr then this function will fail with assertion.
+         */
+        explicit Monitor(GLFWmonitor* monitor);
+
+        /*!
+         * Gets name of the monitor.
+         *
+         * \return The name of the monitor.
+         *
+         * \threadSafety Not thread-safe. Must be called only from the main thread.
+         */
+        [[nodiscard]]
+        std::string_view GetName() const;
+
+        /*!
+         * Get the list of the video modes supported by the monitor.
+         *
+         * \return A list of the video modes supported by the monitor.
+         *
+         * \threadSafety Not thread-safe. Must be called only from the main thread.
+         */
+        [[nodiscard]]
+        const std::vector<VideoMode>& GetVideoModes() const;
+
+        /*!
+         * Selects the specified video mode.
+         *
+         * \param videoModeNumber Index of the video mode within the list of the supported by monitor video modes.
+         *
+         * \attention Index should be less the number of supported video modes by the monitor.
+         *            Otherwise this function will fail with assertion.
+         *
+         * \threadSafety Not thread-safe. Must be called only from the main thread.
+         */
+        void SelectVideoMode(size_t videoModeNumber);
+
+        /*!
+         * Gets the selected video mode.
+         *
+         * \return Currently selected video mode.
+         *
+         * \threadSafety Not thread-safe. Must be called only from the main thread.
+         */
+        [[nodiscard]]
+        const VideoMode& GetSelectedVideoMode() const;
+
+        /*!
+         * Get the size, in millimetres, of the display area of the monitor.
+         *
+         * \return A tuple of two values (width and height).
+         *
+         * \threadSafety Not thread-safe. Must be called only from the main thread.
+         */
+        [[nodiscard]]
+        std::tuple<int32_t, int32_t> GetContentScale() const;
+
+        /*!
+         * Get a raw pointer to the monitor (GLFWmonitor*).
+         *
+         * \return A raw pointer to the GLFWmonitor.
+         *
+         * \threadSafety Not thread-safe. Must be called only from the main thread.
+         */
+        [[nodiscard]]
+        GLFWmonitor* GetHandle() const;
+
+    private:
+        /*! Selected video mode. */
+        size_t _selectedVideoMode = 0;
+        /*! List of supported video modes by the monitor. */
+        std::vector<VideoMode> _videoModes;
+        /*! A raw pointer to the GLFWmonitor. */
+        GLFWmonitor* _monitor = nullptr;
+    };
+}
